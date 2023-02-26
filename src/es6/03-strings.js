1aa28678d440e0e6359d2a10ba55c97b0639af77
Gilberto Espinoza <gilbertoespinoza97@gmail.com>
2023-02-25 22:02:13 -0700

    Implementaciones agregadas en ECMAScript 6 (2017)

---
+++ src/es6/03-strings.js
@@ -0,0 +1,9 @@
+let hello = 'Hello'
+let world = 'World'
+let epicPhrase = hello + ' ' + world + '!';
+console.log(epicPhrase)
+
+// Templete literals
+let mostAmazingPhrase = `${hello} ${world}! `
+console.log(mostAmazingPhrase)
+
